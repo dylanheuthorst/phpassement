71a63db3dfc6439833ddd9f49221cbb7bf46e5bc
dylanheuthorst <64440586+dylanheuthorst@users.noreply.github.com>
2020-06-14 16:42:40 +1200

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <link rel="shortcut icon" type="image/png" href="photos/logo.png">
+    <script src = "animations.js"></script>
+    <title>MyDrugs</title>
+</head>
+<section></section>
+<header>
+    <div id="mySidenav" class="sidenav">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <a href="homepage.php">Home</a>
+        <a href="jff/products.html">Products</a>
+        <a href="#">About Us</a>
+        <a href="jff/faq.html">FAQ</a>
+    </div>
+    <div class = "sandwichHolder" onclick="openNav()">
+        <div class = "bar"></div>
+        <div class = "bar"></div>
+        <div class = "bar"></div>
+    </div>
+</header>
+<img class = "bg" src = "photos/bg.png">
+<body>
+    <img class = "logo" src="photos/logo.png">
+    <img class = "logo1" src="photos/splat.png">
+    <p class = "logotext">MyDrugs</p>
+</body>
+
+<footer>
+</footer>
+</html>
