965c50696ed505b7fb84f4458e83be9cfefffce2
dylanheuthorst <64440586+dylanheuthorst@users.noreply.github.com>
2020-06-18 22:59:58 +1200

    Add files via upload

--- login.php
+++ login.php
@@ -1,22 +1,25 @@
 <?php
-$username = $_POST["username"];
-$password = $_POST["password"];
 
-$host = "qn66usrj1lwdk1cc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$user = "xavpqyym6459viap";
-$password = "eqvb6ev6yq5rogs3";
-$database = "nocjh6h9cdq7i00a";
+if (isset($_POST["username"])){
+    $username = $_POST["username"];
+    $password = $_POST["password"];
 
-$con = new mysqli($host,$user,$password,$database );
+    $host = "qn66usrj1lwdk1cc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $user = "xavpqyym6459viap";
+    $password = "eqvb6ev6yq5rogs3";
+    $database = "nocjh6h9cdq7i00a";
 
-$sql = " select * from logins where Username = '".$username."'AND Password = '".$password."'
-            limit 1";
-$result = mysqli_query($sql);
-if (mysql_num_rows($result)==1){
-    echo "You Have successfully logged in!";
-}else{
-    echo "You have entered an incorrect username or password";
+    $con = new mysqli($host,$user,$password,$database );
+//I will use my login function here
+    if (logins($username,$password)){ // if true (login success)
+        session_start();
+        $_SESSION["username"] = $username;
+
+    }else{ //login fail
+        echo "Wrong username or password";
+    }
 }
+
 
 
 
