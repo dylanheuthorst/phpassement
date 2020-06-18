202e133fd07e64d2a1bd60f321f348f47ba2ad18
dylanheuthorst <64440586+dylanheuthorst@users.noreply.github.com>
2020-06-18 21:32:01 +1200

    Add files via upload

--- Cart.php
+++ Cart.php
@@ -160,8 +160,7 @@ if (isset($_GET["action"])){
             if(!empty($_SESSION["cart"])){
                 foreach ($_SESSION["cart"] as $key => $value) {
                     ?>
-                    <p class="navreceipt"><?php echo $value["item_name"], $value["item_quantity"], $value["product_price"]; ?></p>
-                    }
+                    <p class="navreceipt"><?php echo $value["item_name"]?> x<?php echo $value["item_quantity"]?> $<?php echo $value["product_price"]?></p>
                     <?php
                 }    }
         ?>
