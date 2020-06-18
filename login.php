0c8fd8d57dc2c48c8e6aaf8ac42ce84c99b3a910
dylanheuthorst <64440586+dylanheuthorst@users.noreply.github.com>
2020-06-18 22:57:01 +1200

    Add files via upload

--- login.php
+++ login.php
@@ -1,29 +1,23 @@
 <?php
-if (isset($_POST["username"])){
-    $username = $_POST["username"];
-    $password = $_POST["password"];
+$username = $_POST["username"];
+$password = $_POST["password"];
 
-    $host = "qn66usrj1lwdk1cc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-    $user = "xavpqyym6459viap";
-    $password = "eqvb6ev6yq5rogs3";
-    $database = "nocjh6h9cdq7i00a";
+$host = "qn66usrj1lwdk1cc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$user = "xavpqyym6459viap";
+$password = "eqvb6ev6yq5rogs3";
+$database = "nocjh6h9cdq7i00a";
 
-    $con = new mysqli($host,$user,$password,$database );
+$con = new mysqli($host,$user,$password,$database );
 
-    if ($con->connect_error){
-        echo $con->connect_error;
-    } else{
-        echo "Connection Created";
-    }
-
-//I will use my login function here
-    if (login($username,$password)){ // if true (login success)
-        session_start();
-        $_SESSION["username"] = $username;
-
-    }else{ //login fail
-        echo "Wrong username or password";
-    }
+$sql = " select * from logins where Username = '".$username."'AND Password = '".$password."'
+            limit 1";
+$result = mysqli_query($sql);
+if (mysql_num_rows($result)==1){
+    echo "You Have successfully logged in!";
+}else{
+    echo "You have entered an incorrect username or password";
 }
+
+
 
 /*http://herokugitphpleisong.herokuapp.com/phpMyAdminDylan/
