a409732e53a574d275c630c07b253c1cdd154147
dylanheuthorst <64440586+dylanheuthorst@users.noreply.github.com>
2020-06-18 19:19:30 +1200

    Add files via upload

--- Cart.php
+++ Cart.php
@@ -61,38 +61,17 @@ if (isset($_GET["action"])){
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-    <style>
-        @import url('https://fonts.googleapis.com/css?family=Titillium+Web');
-
-        *{
-            font-family: 'Titillium Web', sans-serif;
-        }
-        .product{
-            border: 1px solid #eaeaec;
-            margin: -1px 19px 3px -1px;
-            padding: 10px;
-            text-align: center;
-            background-color: #efefef;
-        }
-        table, th, tr{
-            text-align: center;
-        }
-        .title2{
-            text-align: center;
-            color: #66afe9;
-            background-color: #efefef;
-            padding: 2%;
-        }
-        h2{
-            text-align: center;
-            color: #66afe9;
-            background-color: #efefef;
-            padding: 2%;
-        }
-        table th{
-            background-color: #efefef;
-        }
-    </style>
+    <div id="mySidenav" class="sidenav">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <a href="index.php">Home</a>
+        <a href="products.php">Products</a>
+        <a href="aboutus.php">About Us</a>
+    </div>
+    <div class = "sandwichHolder" onclick="openNav()">
+        <div class = "bar"></div>
+        <div class = "bar"></div>
+        <div class = "bar"></div>
+    </div>
 </head>
 <body>
 
