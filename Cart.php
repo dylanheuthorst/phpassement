8822c64c73784cd31fa97dc9706417b2120daaab
dylanheuthorst <64440586+dylanheuthorst@users.noreply.github.com>
2020-06-18 20:04:13 +1200

    Add files via upload

--- Cart.php
+++ Cart.php
@@ -89,21 +89,23 @@ if (isset($_GET["action"])){
         while ($row = mysqli_fetch_array($result)) {
 
             ?>
-            <div class="col-md-3">
+            <div class="productHolder">
+                <div class="col-md-3">
 
-                <form method="post" action="Cart.php?action=add&id=<?php echo $row["id"]; ?>">
+                    <form method="post" action="Cart.php?action=add&id=<?php echo $row["id"]; ?>">
 
-                    <div class="product">
-                        <img src="<?php echo $row["image"]; ?>" class="img-responsive">
-                        <h5 class="text-info"><?php echo $row["pname"]; ?></h5>
-                        <h5 class="text-danger"><?php echo $row["price"]; ?></h5>
-                        <input type="text" name="quantity" class="form-control" value="1">
-                        <input type="hidden" name="hidden_name" value="<?php echo $row["pname"]; ?>">
-                        <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>">
-                        <input type="submit" name="add" style="margin-top: 5px;" class="btn btn-success"
-                               value="Add to Cart">
-                    </div>
-                </form>
+                        <div class="product">
+                            <img src="<?php echo $row["image"]; ?>" class="img-responsive">
+                            <h5 class="text-info"><?php echo $row["pname"]; ?></h5>
+                            <h5 class="text-danger"><?php echo $row["price"]; ?></h5>
+                            <input type="text" name="quantity" class="form-control" value="1">
+                            <input type="hidden" name="hidden_name" value="<?php echo $row["pname"]; ?>">
+                            <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>">
+                            <input type="submit" name="add" style="margin-top: 5px;" class="btn btn-success"
+                                   value="Add to Cart">
+                        </div>
+                    </form>
+                </div>
             </div>
             <?php
         }
