70daaf9b1c46a25832c0deeff0193c7ca1c16557
dylanheuthorst <64440586+dylanheuthorst@users.noreply.github.com>
2020-06-18 18:01:37 +1200

    Add files via upload

--- createdb.php
+++ createdb.php
@@ -1,5 +1,4 @@
 <?php
-
     $host = "qn66usrj1lwdk1cc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
     $user = "xavpqyym6459viap";
     $password = "eqvb6ev6yq5rogs3";
@@ -18,5 +17,11 @@
     $createemail = $_POST["createemail"];
     $createpwd = $_POST["createpwd"];
 
-    $newUser = new User(0,$createname,$createusername,$createemail,$createpwd);
-    $newUser->save();
+    $sql = "INSERT INTO logins (ClientID,Name,Username,Email,Password) VALUES (0,'$createname','$createusername','$createemail','$createpwd')";
+
+    if (mysqli_query($con,$sql)){
+        echo "Not Inserted";
+    }
+    else{
+        echo "Inserted";
+    }
